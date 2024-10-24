0f1ed8633c4e5e0d3014f3c4efc7ae18e5b51001
Amdadullah-Raihan <amdadullahrayhan@gmail.com>
2024-10-24 17:42:04 +0600

    feat: add banner on agent page

--- src/pages/agent/AgentPage.jsx
+++ src/pages/agent/AgentPage.jsx
@@ -60,42 +60,79 @@ const AgentPage = () => {
       {error && <p className="text-red-500">{error}</p>}
 
       {!loading && !error && (
-        <div className="grid gap-4 sm:grid-cols-2">
-          {agents.length > 0 ? (
-            agents.map((agent) => (
-              <div
-                key={agent.id}
-                className="group relative border  rounded-md shadow-md overflow-hidden"
-              >
-                {/* Display a random image for each agent */}
+        <>
+          <div className="bg-gray-950 mb-10 rounded-lg text-white flex justify-between gap-4">
+            <div className="p-8">
+              <p className="text-3xl">Create your agent</p>
+              <p className="opacity-65 mt-6">
+                Craft a unique AI agent of your own by uploading your data and{" "}
+                <br />
+                effortlessly customizing its appearance and personality.
+              </p>
+            </div>
+            <div className="w-1/3 flex items-center gap-6 justify-end mr-4">
+              <div className="space-y-4 py-3">
                 <img
-                  src={getRandomImage()}
-                  alt="Agent"
-                  className="w-full h-48 object-cover rounded-md "
+                  src={
+                    "https://img.freepik.com/free-photo/cartoon-ai-robot-scene_23-2151675076.jpg?ga=GA1.1.522172574.1729754806&semt=ais_hybrid"
+                  }
+                  alt=""
+                  className="max h-20 rounded-lg shadow-[0px_0px_5px_1px_cyan]"
                 />
-                {/* Overlay content - hidden by default, shown on hover */}
-                <div className="absolute inset-0 flex flex-col justify-center items-center bg-black bg-opacity-30 hover:bg-opacity-60 text-white  opacity-100 transition duration-300 p-4 rounded-md">
-                  <h2 className="font-semibold text-lg mb-2">
-                    {agent.agent_name}
-                  </h2>
-                  <CButton
-                    variant={"outline"}
-                    // loading={isLoading}
-                  >
-                    Start Conversation
-                  </CButton>
-                </div>
+                <img
+                  src={
+                    "https://img.freepik.com/free-vector/graident-ai-robot-vectorart_78370-4114.jpg?t=st=1729769273~exp=1729772873~hmac=7dd547cf4755352b4f17367f9247d38f8ef10f7932b02f4b6ba5e1f002b76399&w=740"
+                  }
+                  alt=""
+                  className="max h-20 rounded-lg shadow-[0px_0px_5px_1px_cyan]"
+                />
               </div>
-            ))
-          ) : (
-            <CButton
-              onClick={handleCreateAgent}
-              className="border py-24 hover:bg-orange-500 overflow-hidden rounded-lg transition-all duration-300 hover:text-white"
-            >
-              <BsPlus size={22} /> Create Agent
-            </CButton>
-          )}
-        </div>
+              <img
+                src={
+                  "https://images.unsplash.com/photo-1535378620166-273708d44e4c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGFpJTIwcm9ib3R8ZW58MHx8MHx8fDA%3D"
+                }
+                alt=""
+                className="max-h-28 rounded-lg  shadow-[0px_0px_5px_1px_cyan]"
+              />
+            </div>
+          </div>
+          <div className="grid gap-4 sm:grid-cols-2">
+            {agents.length > 0 ? (
+              agents.map((agent) => (
+                <div
+                  key={agent.id}
+                  className="group relative border  rounded-md shadow-md overflow-hidden"
+                >
+                  {/* Display a random image for each agent */}
+                  <img
+                    src={getRandomImage()}
+                    alt="Agent"
+                    className="w-full h-48 object-cover rounded-md "
+                  />
+                  {/* Overlay content - hidden by default, shown on hover */}
+                  <div className="absolute inset-0 flex flex-col justify-center items-center bg-black bg-opacity-30 hover:bg-opacity-60 text-white  opacity-100 transition duration-300 p-4 rounded-md">
+                    <h2 className="font-semibold text-lg mb-2">
+                      {agent.agent_name}
+                    </h2>
+                    <CButton
+                      variant={"outline"}
+                      // loading={isLoading}
+                    >
+                      Start Conversation
+                    </CButton>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <CButton
+                onClick={handleCreateAgent}
+                className="border py-24 hover:bg-orange-500 overflow-hidden rounded-lg transition-all duration-300 hover:text-white"
+              >
+                <BsPlus size={22} /> Create Agent
+              </CButton>
+            )}
+          </div>
+        </>
       )}
     </div>
   );
